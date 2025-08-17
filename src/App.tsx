d6ed614390c999c62db51981a8b60c52c3c634f9
Akal <akal@Akals-Mac-mini.local>
2025-08-17 10:29:10 +0700

    data deletin ios

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import NotFound from "./pages/NotFound";
 import Terms from "./pages/Terms";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import DataDeletion from "./pages/DataDeletion";
+import DataDeletionIOS from "./pages/DataDeletionIOS";
 
 const queryClient = new QueryClient();
 
@@ -22,6 +23,7 @@ const App = () => (
           <Route path="/terms" element={<Terms />} />
           <Route path="/privacy" element={<PrivacyPolicy />} />
           <Route path="/data-deletion" element={<DataDeletion />} />
+          <Route path="/data-deletion-ios" element={<DataDeletionIOS  />} />
 
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
